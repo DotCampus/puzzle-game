28f48ec28bc97fe952f1f760147b040c80caa63e
Theo Okafor <theo.okafor@yahoo.com>
2022-03-10 18:44:15 +0100

    add the puzzle game

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # puzzle-game
 A Sliding Puzzle Game
+
+## Getting started
+
+Run `npm install`
+
+Run `npm run tailwind`
+
+Open the `public/index.html` file on your browser. Alternatively, use live server to run the file.
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#EF8354',
+      'blue': '#475D75',
+      'dark-blue': '#2D3142',
+      'grey': '#BFC0C0',
+      'white': '#FFFFFF',
+      'black': '#000000'
+
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
